f5fccc118c530fa34d9b92a1badd4698f923f1f0
Matheus Santos <matheus.santos@bsempresas.com.br>
2025-03-13 00:00:55 -0300

    feat: add client-side authentication check in middleware for improved navigation

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -3,7 +3,13 @@ import { useAuth } from "~/composables/useAuth";
 export default defineNuxtRouteMiddleware(() => {
   const { isAuthenticated } = useAuth();
 
-  if (!isAuthenticated.value) {
-    return navigateTo("/auth/login");
+  if (import.meta.client) {
+    console.log("isAuthenticated (client side):", isAuthenticated.value);
+
+    if (!isAuthenticated.value) {
+      return navigateTo("/auth/login");
+    }
   }
+
+  return;
 });
